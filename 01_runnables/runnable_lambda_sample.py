fbea29d7ee37b5e9032a9babf6ac2409afd20671
Zhongmin Ma <ma65536@gmail.com>
2025-12-17 18:49:58 -0500

    fix warning

--- 01_runnables/runnable_lambda_sample.py
+++ 01_runnables/runnable_lambda_sample.py
@@ -1,7 +1,7 @@
 from langchain_core.runnables import RunnableLambda
 from llm import gemini_llm
 
-#
 llm = RunnableLambda(gemini_llm)
-print(llm.invoke("Respond with OK only"))
-print(llm.invoke("when gemini release year"))
+
+print(llm.invoke("Respond with OK"))
+print(llm.invoke("When was Gemini released?"))
--- llm.py
+++ llm.py
@@ -1,13 +1,14 @@
+from typing import Any
 from google import genai
 from google.genai import types
 client = genai.Client()
 # text generation docs: https://ai.google.dev/gemini-api/docs/text-generation?authuser=2
-def gemini_llm(prompt: str) -> str:
+def gemini_llm(prompt: Any) -> Any:
     response = client.models.generate_content(
         model="models/gemini-flash-latest",
         contents=prompt,
         config=types.GenerateContentConfig(
-            temperature=0.2,
+            temperature=0.5,
         ),
     )
     return response.text
