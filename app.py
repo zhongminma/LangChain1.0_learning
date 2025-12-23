545b105b65c8a3a3fa43b03ed9f8e5d9ad0b9332
Zhongmin Ma <ma65536@gmail.com>
2025-12-23 01:39:12 -0500

    add reference for app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from langchain_core.prompts import PromptTemplate
 from langserve import add_routes
-
 from llm import llm
 from fastapi import FastAPI
 
@@ -13,6 +12,7 @@ app = FastAPI(
     version="1.0",
     description="Simple LangChain Server"
 )
+
 add_routes(app, chain, path="/explain")
 # add_routes(app, gemini_chain, path="/openai")
 # add_routes(app, gemini_chain, path="/gemini")
@@ -20,3 +20,5 @@ add_routes(app, chain, path="/explain")
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+# http://127.0.0.1:8000/docs for swagger
+# http://127.0.0.1:8000/explain/playground/ for web ui
